8c4097605ce34146e88571b45f3cf95aea87a463
senthilmurugan <163536270+Senthilmurugan-Technospurs@users.noreply.github.com>
2024-11-29 12:13:24 +0530

    code update for responsive screen

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,12 +1,20 @@
-import Link from 'next/link';
-import Image from 'next/image';
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
-import MainHeaderBackground from './main-header-background';
-import logoImg from '@/assets/logo.png';
-import classes from './main-header.module.css';
-import NavLink from './nav-link';
+import MainHeaderBackground from "./main-header-background";
+import logoImg from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
+  const [isMobile, setIsMobile] = useState(false); // State for toggling mobile menu
+
+  const toggleMenu = () => {
+    setIsMobile(!isMobile); // Toggle the mobile menu state
+  };
+
   return (
     <>
       <MainHeaderBackground />
@@ -16,16 +24,48 @@ export default function MainHeader() {
           NextLevel Food
         </Link>
 
-        <nav className={classes.nav}>
-          <ul>
-            <li>
-              <NavLink href="/meals">Browse Meals</NavLink>
-            </li>
-            <li>
-              <NavLink href="/community">Foodies Community</NavLink>
-            </li>
-          </ul>
-        </nav>
+        {/* Hamburger icon */}
+        <div className={classes.hamburger} onClick={toggleMenu}>
+          <span className={classes.bar}></span>
+          <span className={classes.bar}></span>
+          <span className={classes.bar}></span>
+        </div>
+
+        {/* Navigation links */}
+        {/* Web View */}
+        {!isMobile && (
+          <nav className={classes.nav}>
+            <ul>
+              <li>
+                <NavLink href="/meals">Browse Meals</NavLink>
+              </li>
+              <li>
+                <NavLink href="/community">Foodies Community</NavLink>
+              </li>
+            </ul>
+          </nav>
+        )}
+         {/* Mobile View */}
+        {isMobile && (
+          <nav className={classes.nav}>
+            <ul
+              className={`${isMobile ? classes.navList : ""} ${
+                isMobile ? classes.active : ""
+              }`}
+            >
+              <li onClick={()=>{
+                setIsMobile(!isMobile);
+              }}>
+                <NavLink href="/meals">Browse Meals</NavLink>
+              </li>
+              <li onClick={()=>{
+                setIsMobile(!isMobile);
+              }}>
+                <NavLink href="/community">Foodies Community</NavLink>
+              </li>
+            </ul>
+          </nav>
+        )}
       </header>
     </>
   );
